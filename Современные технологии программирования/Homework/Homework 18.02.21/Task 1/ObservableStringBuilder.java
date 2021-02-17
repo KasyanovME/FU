8a86ea9fe22910123fbdb65b8750b92c9a80b7fa
Maxim Kasyanov <70855034+KasyanovME@users.noreply.github.com>
2021-02-17 21:22:11 +0300

    Add homework 18.02.21

---
+++ Современные технологии программирования/Homework/Homework 18.02.21/Task 1/ObservableStringBuilder.java
@@ -0,0 +1,53 @@
+package com.company;
+
+class ObservableStringBuilder {
+
+    private StringBuilder delegateBuilder;
+    private OnStringChangedListener onChangeListener;
+
+    ObservableStringBuilder() {
+        delegateBuilder = new StringBuilder();
+    }
+
+    ObservableStringBuilder(String string) {
+        delegateBuilder = new StringBuilder(string);
+    }
+
+    void setOnChangeListener(OnStringChangedListener onChangeListener) {
+        this.onChangeListener = onChangeListener;
+    }
+
+    ObservableStringBuilder append(Object obj) {
+        delegateBuilder.append(obj);
+        notifyListeners();
+        return this;
+    }
+
+    public ObservableStringBuilder insert(int index, char[] str, int offset, int len) {
+        delegateBuilder.insert(index, str, offset, len);
+        notifyListeners();
+        return this;
+    }
+
+    public ObservableStringBuilder replace(int start, int end, String str) {
+        delegateBuilder.replace(start, end, str);
+        notifyListeners();
+        return this;
+    }
+
+    public ObservableStringBuilder delete(int start, int day) {
+        delegateBuilder.delete(start, day);
+        notifyListeners();
+        return this;
+    }
+
+    private void notifyListeners() {
+        if (onChangeListener != null)
+            onChangeListener.onStringChanged(this);
+    }
+
+    @Override
+    public String toString() {
+        return delegateBuilder.toString();
+    }
+}
