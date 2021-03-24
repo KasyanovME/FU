3042e9f519b9ab8330fc5ff974daa1c291b94808
Maxim Kasyanov <70855034+KasyanovME@users.noreply.github.com>
2021-03-24 19:25:48 +0300

    Rest API

---
+++ Современные технологии программирования/rest_pi19_4/src/test/java/com/rest_pi_19_4/rest_pi_19_4/RestPi194ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest_pi_19_4.rest_pi_19_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPi194ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
